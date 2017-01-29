d7b711ce8ce5dfaddb000cf1a5503b86da825538
bloodaccess <blood.access@gmail.com>
2017-01-29 20:23:52 +0100

    test commit

--- application/vues/projets.php
+++ application/vues/projets.php
@@ -3,46 +3,46 @@ require_once './configs/chemins.class.php';
 require Chemins::VUES_PERMANENTE.'head.inc.php';
 ?>
 
-    <body>
-        
-        <div id="page_projet">
-            
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-               <h3>portefolio V.2</h3>
-                <p><img src="images/projet.JPG" class="img-responsive"><br> voici une image de mon site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP. Pour ce site la base est la template ZOZORE du site du zero</p>
-            </div>
+<body>
 
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-               <h3>Potefolio V.1</h3>
-                <p><img src="images/portefolioV1.JPG" class="img-responsive"><br> voici une image de mon ancien site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP. Pour ce site c'est un creation complete.</p>
-            </div>
+<div id="page_projet">
 
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-               <h3>The Gold Number</h3>
-                <p><img src="images/projet_3.JPG" class="img-responsive"><br> voici une image de l'application The Gold Number. cette application develloper en VB.NET consiste a généré un nombre aleatoire entre 1 et 10 et l'utilisateur dois trouver le bon nombre en un minimum de coup.</p>
-            </div>
-            
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-               <h3>Virtual Box</h3>
-                <p><img src="images/projet_4.JPG" class="img-responsive"><br> voici une image de mon site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP.</p>
-            </div>
-            
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-               <h3>Cisco Pacquet tracer</h3>
-                <p><img src="images/projet_5.JPG" class="img-responsive"><br> voici une image de mon site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP.</p>
-            </div>
-            
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-                <h3>Recherche DEEZER V.1</h3>
-                <p><img src="images/DeezerV1.JPG" class="img-responsive"><br> voici une image de l'application Recherche DEEZER. cette application en C# consiste a travailler avec les API de DEEZER.</p>
-            </div>
-            
-            <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
-                 <h3>Recherche DEEZER V.2</h3>
-                    <p><img src="images/projet_2.JPG" class="img-responsive"><br> voici une image de l'application Recherche DEEZER. cette application en C# consiste a travailler avec les API de DEEZER.</p>
-            </div>
-            <br>
-            <br>
-        <?php include_once("./php/analyticstracking.php")// statistique google  ?> 
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>portefolio V.2</h3>
+        <p><img src="images/projet.JPG" class="img-responsive"><br> voici une image de mon site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP. Pour ce site la base est la template ZOZORE du site du zero</p>
     </div>
+
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>Potefolio V.1</h3>
+        <p><img src="images/portefolioV1.JPG" class="img-responsive"><br> voici une image de mon ancien site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP. Pour ce site c'est un creation complete.</p>
+    </div>
+
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>The Gold Number</h3>
+        <p><img src="images/projet_3.JPG" class="img-responsive"><br> voici une image de l'application The Gold Number. cette application develloper en VB.NET consiste a généré un nombre aleatoire entre 1 et 10 et l'utilisateur dois trouver le bon nombre en un minimum de coup.</p>
+    </div>
+
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>Virtual Box</h3>
+        <p><img src="images/projet_4.JPG" class="img-responsive"><br> voici une image de mon site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP.</p>
+    </div>
+
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>Cisco Pacquet tracer</h3>
+        <p><img src="images/projet_5.JPG" class="img-responsive"><br> voici une image de mon site internet. il est coder en HTML / CCS 3 et intègre quelque partie en PHP.</p>
+    </div>
+
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>Recherche DEEZER V.1</h3>
+        <p><img src="images/DeezerV1.JPG" class="img-responsive"><br> voici une image de l'application Recherche DEEZER. cette application en C# consiste a travailler avec les API de DEEZER.</p>
+    </div>
+
+    <div id="projet_1" class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <h3>Recherche DEEZER V.2</h3>
+        <p><img src="images/projet_2.JPG" class="img-responsive"><br> voici une image de l'application Recherche DEEZER. cette application en C# consiste a travailler avec les API de DEEZER.</p>
+    </div>
+    <br>
+    <br>
+    <?php include_once("./php/analyticstracking.php")// statistique google  ?>
+</div>
 </body>
